57b0d0fc90305acbb3576e9327996b323cd70757
sadatakhtar <sadat0akhtar@gmail.com>
2020-12-10 22:11:29 +0000

    Created login page

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -3,8 +3,23 @@ import './LoginComponent.css';
 
 function LoginComponent() {
     return (
-        <div>
-            <h1>Login page</h1>
+        <div className="login_container">
+            <div className="login_title">
+                <h2>Log In</h2>
+            </div>
+            <div className="login_form">
+                <form>
+                    <input name="username" type="text" placeholder="Username" required /> 
+                    <input name="password" type="password" placeholder="Password" required /> 
+                    <div className="login_form_btn">
+                        <button type="submit">Login</button>
+                    </div>
+                    <div className="login_btn_links">
+                        <p>Sign up | Forgot password?</p>
+                    </div>
+                </form>
+            </div>
+            
         </div>
     )
 }
